d96958aa707c210a4f4d32aef4a00249d0a0c290
David Schwarz <zeraphil@gmail.com>
2014-07-05 16:07:53 -0400

    Initial Commit from SVN to Git.

---
+++ EmoBlaster/EmoBlaster/GameRoot.cs
@@ -0,0 +1,220 @@
+﻿//---------------------------------------------------------------------------------
+// Written by Michael Hoffman
+// Find the full tutorial at: http://gamedev.tutsplus.com/series/vector-shooter-xna/
+//----------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using BloomPostprocess;
+using Emotiv;
+
+namespace ShapeBlaster
+{
+	public class GameRoot : Microsoft.Xna.Framework.Game
+	{
+		// some helpful static properties
+		public static GameRoot Instance { get; private set; }
+		public static Viewport Viewport { get { return Instance.GraphicsDevice.Viewport; } }
+		public static Vector2 ScreenSize { get { return new Vector2(Viewport.Width, Viewport.Height); } }
+		public static GameTime GameTime { get; private set; }
+		public static ParticleManager<ParticleState> ParticleManager { get; private set; }
+		public static Grid Grid { get; private set; }
+        public static Single engagement { get; set; }
+        public static Single emoPower { get; set; }
+
+        void engine_UserRemoved(object sender, EmoEngineEventArgs e)
+        {
+            Console.WriteLine("User Removed !");
+        }
+
+        void engine_UserAdded(object sender, EmoEngineEventArgs e)
+        {
+            Console.WriteLine("User Added !");
+        }
+
+        void engine_EmoStateUpdated(object sender, EmoStateUpdatedEventArgs e)
+        {
+            Console.WriteLine("EmoState Updated !");
+        }
+
+        static void engine_CognitivEmoStateUpdated(object sender, EmoStateUpdatedEventArgs e)
+        {
+            EmoState es = e.emoState;
+
+            Single timeFromStart = es.GetTimeFromStart();
+
+            EdkDll.EE_CognitivAction_t cogAction = es.CognitivGetCurrentAction();
+            Single power = es.CognitivGetCurrentActionPower();
+            Boolean isActive = es.CognitivIsActive();
+            emoPower = power;
+        }
+
+        static void engine_AffectivEmoStateUpdated(object sender, EmoStateUpdatedEventArgs e)
+        {
+            EmoState es = e.emoState;
+
+            Single timeFromStart = es.GetTimeFromStart();
+
+            EdkDll.EE_AffectivAlgo_t[] affAlgoList = { 
+                                                      EdkDll.EE_AffectivAlgo_t.AFF_ENGAGEMENT_BOREDOM,
+                                                      EdkDll.EE_AffectivAlgo_t.AFF_EXCITEMENT,
+                                                      EdkDll.EE_AffectivAlgo_t.AFF_FRUSTRATION,
+                                                      EdkDll.EE_AffectivAlgo_t.AFF_MEDITATION,
+                                                      };
+
+            Boolean[] isAffActiveList = new Boolean[affAlgoList.Length];
+
+            Single longTermExcitementScore = es.AffectivGetExcitementLongTermScore();
+            Single shortTermExcitementScore = es.AffectivGetExcitementShortTermScore();
+            for (int i = 0; i < affAlgoList.Length; ++i)
+            {
+                isAffActiveList[i] = es.AffectivIsActive(affAlgoList[i]);
+            }
+            Single meditationScore = es.AffectivGetMeditationScore();
+            Single frustrationScore = es.AffectivGetFrustrationScore();
+            Single boredomScore = es.AffectivGetEngagementBoredomScore();
+
+            engagement = (boredomScore + longTermExcitementScore) / 2.0f;
+
+        }
+
+		GraphicsDeviceManager graphics;
+		SpriteBatch spriteBatch;
+		BloomComponent bloom;
+        EmoEngine engine = EmoEngine.Instance;
+
+		bool paused = false;
+		bool useBloom = true;
+
+		public GameRoot()
+		{
+			Instance = this;
+			graphics = new GraphicsDeviceManager(this);
+			Content.RootDirectory = "Content";
+
+			graphics.PreferredBackBufferWidth = 800;
+			graphics.PreferredBackBufferHeight = 600;
+
+			bloom = new BloomComponent(this);
+			Components.Add(bloom);
+			bloom.Settings = new BloomSettings(null, 0.25f, 4, 2, 1, 1.5f, 1);
+		}
+
+		protected override void Initialize()
+		{
+			base.Initialize();
+            engine.UserAdded += new EmoEngine.UserAddedEventHandler(engine_UserAdded);
+            engine.EmoStateUpdated += new EmoEngine.EmoStateUpdatedEventHandler(engine_EmoStateUpdated);
+            engine.UserRemoved += new EmoEngine.UserRemovedEventHandler(engine_UserRemoved);
+            engine.CognitivEmoStateUpdated += new EmoEngine.CognitivEmoStateUpdatedEventHandler(engine_CognitivEmoStateUpdated);
+            engine.AffectivEmoStateUpdated += new EmoEngine.AffectivEmoStateUpdatedEventHandler(engine_AffectivEmoStateUpdated);
+
+            engine.RemoteConnect("127.0.0.1", 3008);
+
+			ParticleManager = new ParticleManager<ParticleState>(1024 * 20, ParticleState.UpdateParticle);
+
+			const int maxGridPoints = 1600;
+			Vector2 gridSpacing = new Vector2((float)Math.Sqrt(Viewport.Width * Viewport.Height / maxGridPoints));
+			Grid = new Grid(Viewport.Bounds, gridSpacing);
+
+			EntityManager.Add(PlayerShip.Instance);
+
+			MediaPlayer.IsRepeating = true;
+			MediaPlayer.Play(Sound.Music);
+		}
+
+		protected override void LoadContent()
+		{
+			spriteBatch = new SpriteBatch(GraphicsDevice);
+			Art.Load(Content);
+			Sound.Load(Content);
+		}
+
+		protected override void Update(GameTime gameTime)
+		{
+			GameTime = gameTime;
+			Input.Update();
+			// Allows the game to exit
+			if (Input.WasButtonPressed(Buttons.Back) || Input.WasKeyPressed(Keys.Escape))
+				this.Exit();
+
+			if (Input.WasKeyPressed(Keys.P))
+				paused = !paused;
+			if (Input.WasKeyPressed(Keys.B))
+				useBloom = !useBloom;
+
+			if (!paused)
+			{
+                engine.ProcessEvents(1000);
+				PlayerStatus.Update();
+                EntityManager.Update(engagement, emoPower);
+				EnemySpawner.Update();
+				ParticleManager.Update();
+				Grid.Update();
+			}
+
+			base.Update(gameTime);
+		}
+
+		protected override void Draw(GameTime gameTime)
+		{
+			bloom.BeginDraw();
+			if (!useBloom)
+				base.Draw(gameTime);
+
+			GraphicsDevice.Clear(Color.Black);
+
+			spriteBatch.Begin(SpriteSortMode.Texture, BlendState.Additive);
+			EntityManager.Draw(spriteBatch);
+			spriteBatch.End();
+
+			spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.Additive);
+
+            Color color = ColorUtil.HSVToColor((6f * (1-engagement))/2f, .5f, .8f);
+            Color gridColor = new Color(color.R, color.G, color.B, 120);
+			Grid.Draw(gridColor, spriteBatch);
+			ParticleManager.Draw(spriteBatch);
+			spriteBatch.End();
+
+			if (useBloom)
+				base.Draw(gameTime);
+
+			// Draw the user interface without bloom
+			spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.Additive);
+
+			spriteBatch.DrawString(Art.Font, "Lives: " + PlayerStatus.Lives, new Vector2(5), Color.White);
+			DrawRightAlignedString("Score: " + PlayerStatus.Score, 5);
+			DrawRightAlignedString("Multiplier: " + PlayerStatus.Multiplier, 35);
+			// draw the custom mouse cursor
+			spriteBatch.Draw(Art.Pointer, Input.MousePosition, Color.White);
+
+			if (PlayerStatus.IsGameOver)
+			{
+				string text = "Game Over\n" +
+					"Your Score: " + PlayerStatus.Score + "\n" +
+					"High Score: " + PlayerStatus.HighScore;
+
+				Vector2 textSize = Art.Font.MeasureString(text);
+				spriteBatch.DrawString(Art.Font, text, ScreenSize / 2 - textSize / 2, Color.White);
+			}
+
+			spriteBatch.End();
+		}
+
+		private void DrawRightAlignedString(string text, float y)
+		{
+			var textWidth = Art.Font.MeasureString(text).X;
+			spriteBatch.DrawString(Art.Font, text, new Vector2(ScreenSize.X - textWidth - 5, y), Color.White);
+		}
+	}
+}
